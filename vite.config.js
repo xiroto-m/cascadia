a01aa2ec9e76865ab5efd8109168ad9e0427e621
xiroto-m <hiroto.mizutani@gmail.com>
2026-07-20 11:22:02 +0900

    chore: update vite configuration for path resolution and proxy settings

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       input: {
         main: './index.html',
